91ce8cad5ec405182686c739721b3f697a567d32
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-02-01 01:23:36 +0000

    BIG step for me,Small step for humanity. Client talks to server now, Canvas gets coords.

--- client/src/components/BlockGame.vue
+++ client/src/components/BlockGame.vue
@@ -7,19 +7,32 @@
 </template>
 
 <script>
-    //import io from 'socket.io-client'; 
+    import io from 'socket.io-client'; 
     
     export default {
      name: 'BlockGame',
      data(){
          return {
              socket: {},
-             //context,
+             context: 0,
              position :{
                  x: 0,
                  y: 0
                 }
             }
+        },
+        // Ready to establish a socket connection, best way is created(), before the View renders
+        created(){
+            this.socket = io("https://3000-ec7c0a46-d8e8-4fb7-b436-551bbd8a6fdc.ws-eu01.gitpod.io/"); // Client socket to > server adress
+        },
+        // After the view renders, we want to start listening for events, best way is mounted(), so we can work with our canvas
+        mounted(){
+            this.context = this.$refs.game.getContext("2d");
+            this.socket.on("position", data => {
+                this.position = data;
+                this.context.fillRect(this.position.x, this.position.y, 20, 20);
+            })
+           
         }
     }
 </script>
