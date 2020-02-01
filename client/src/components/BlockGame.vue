1665ed5f21b331fc1e47f837c2704bf871033399
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-02-01 01:07:36 +0000

    Setup done. Before socket connections.

---
+++ client/src/components/BlockGame.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <canvas ref="game" width="640" height="480" style="border: 1px solid black;">
+    
+    </canvas>
+  </div>
+</template>
+
+<script>
+    //import io from 'socket.io-client'; 
+    
+    export default {
+     name: 'BlockGame',
+     data(){
+         return {
+             socket: {},
+             //context,
+             position :{
+                 x: 0,
+                 y: 0
+                }
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
